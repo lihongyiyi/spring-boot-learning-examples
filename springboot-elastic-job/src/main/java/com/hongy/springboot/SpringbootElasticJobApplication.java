d157b7b52d8c67db45c62bf08d9af1eace666f3a
lihongy <lihongying@videopls.com>
2019-06-04 15:09:50 +0800

    初始化项目

---
+++ springboot-elastic-job/src/main/java/com/hongy/springboot/SpringbootElasticJobApplication.java
@@ -0,0 +1,13 @@
+package com.hongy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticJobApplication.class, args);
+	}
+
+}
